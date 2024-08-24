20f442256799b00a73c6cb491b8fc45d9cbe5528
Sean Smith <pirsquared.snv@gmail.com>
2024-08-24 07:37:35 -0700

    added comments

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
-from pybricks.hubs import InventorHub
-from pybricks.parameters import Direction, Port
+from pybricks.hubs import PrimeHub
+from pybricks.parameters import Axis, Direction, Port
 from pybricks.pupdevices import Motor
 from pybricks.robotics import DriveBase
 
 # Set up all devices.
-inventor_hub = InventorHub()
-left_wheel = Motor(Port.A, Direction.COUNTERCLOCKWISE)
-right_wheel = Motor(Port.B, Direction.CLOCKWISE)
-drive_base = DriveBase(left_wheel, right_wheel, 56, 195)
-drive_base.use_gyro(True)
+# The top side is the side with the light matrix
+# The front side is the side with the usb port
+# Setting up the hub appropriately allows for you to use the gyro for turning
+# Axes are:
+# `Axis.X` is forward, `-Axis.X` is backward
+# `Axis.Y` is right, `-Axis.Y` is left
+# `Axis.Z` is up, `-Axis.Z` is down
+# Currently, the bot is built with the 'top_side' facing backward
+# therefore we set the `top_side` argument to `-Axis.X`
+# Currently, the bot is built with the 'front_side' facing up
+# therefore we set the `front_side` argument to `Axis.Z`
+hub = PrimeHub(top_side=-Axis.X, front_side=Axis.Z)
+left = Motor(Port.E, Direction.COUNTERCLOCKWISE)
+rite = Motor(Port.F, Direction.CLOCKWISE)
+base = DriveBase(left, rite, 56, 114)
 
 
 # The main program starts here.
@@ -17,4 +27,6 @@ print('''\
 ### Hello Pybricks! ############################
 ################################################
 ''')
-drive_base.turn(1080)
+base.use_gyro(True)
+base.straight(100)
+base.turn(180)
