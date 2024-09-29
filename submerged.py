d6215e0d5e8601e0feca3f7acf5c3708378df66e
Sean Smith <1900410+pirsquared@users.noreply.github.com>
2024-09-29 08:41:50 -0700

    Update submerged.py

--- submerged.py
+++ submerged.py
@@ -7,14 +7,14 @@ from LegoLegacy import Bot, clear_console
 
 class GameBot(Bot):
     def _13(self, reverse=False):
-        distance = -160 if reverse else 160
-        self.liftby(1.5)
-        self.strait(100, speed=400)
+        distance = -157 if reverse else 157
+        # self.liftby(2)
+        # self.strait(100, speed=400)
         self.twist_turn(90, trn_rt=200, trn_acc=200)
-        self.move_lift_sine(distance, 180, 11, 2000)
+        self.move_lift_sine(distance, 720, 12, 4000)
         self.twist_turn(-90, trn_rt=200, trn_acc=200)
-        self.liftby(-1.5)
-        return self.strait(-100, speed=400)
+        # self.liftby(-1.5)
+        # return self.strait(-100, speed=400)
 
 clear_console()
 bot = GameBot(
@@ -26,8 +26,25 @@ bot = GameBot(
     right_eye=Port.D,
     hub_type=PrimeHub
 )
-
-bot._13()
+i = 1
+# bot._13(reverse=i==0)
+# bot.twist_turn(90, trn_rt=200, trn_acc=200)
+# bot.straight(-40)
+# bot.liftby(7)
+# bot.straight(20)
+# bot.liftby(3)
+# bot.straight(56)
+# bot.liftby(-28, speed=3000)
+sw = StopWatch()
+bot.lift_sine_unit(3000, 14, 1000)
+print(sw.time())
+# bot.lift.reset_angle(0)
+# bot.lift.run_angle(720, 360)
+# print(bot.lift.angle())
+# bot.straight(56)
+# bot.lift.reset_angle(0)
+# bot.liftto(2)
+# bot.liftto(-2)
 # bot.lift_sine_unit(180, 13, 2000)
 # bot.lift_sine_unit(180, 13, 2000)
 # bot.lift_sine_unit(180, 13, 2000)
